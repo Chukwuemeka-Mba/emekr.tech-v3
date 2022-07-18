e94559e8b3643816193460ddea8d83de1d191c3c
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-07-18 09:03:51 -0400

    :sparkles: added bio section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
-import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react"
+import { Container, Link, Button, Box, Heading, Paragraph, Image, useColorModeValue } from "@chakra-ui/react"
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import NextLink from 'next/link'
+import Section from "../components/section"
+import ParagraphCard from "../components/paragraph-card"
+import { BioSection, BioYear } from "../components/bio"
+
 
 const Page = () => {
     return (
@@ -11,15 +17,15 @@ const Page = () => {
         w="100%" 
         align="center"
         >
-          Hello World! I am a fullstack developer based in Boston Massachusetts.
-        </Box>
+     Game Changer
+       </Box>
         <Box display={{md: 'flex'}}>
           <Box flexGrow={1} 
         align="center">
             <Heading as="h2" variant="page-title">
               Chukwuemeka Mba
             </Heading>
-            <p>Digital artist, engineer and designer. Writer.</p>
+            <p>Software engineer, publisher.</p>
           </Box>
           <Box flexShrink={0} mt={{base: 4, md: 0}} ml={{ md: 6}} align="center">
           <Image
@@ -33,6 +39,46 @@ const Page = () => {
           />
           </Box>
         </Box>
+          <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <ParagraphCard>
+      I am Emeka, a full stack software engineer based in Massachusetts USA. 
+      I work closely with frontend teams to build unique user experiences for my clients’ products. 
+      I bring a unique component based approach to frontend development which accelerates build time and frees teams to be their best selves  while working hard to achieve great feats.
+              
+      
+      Currently working on a project called {"- "} 
+        <NextLink href="/">
+          <Link>Project</Link>
+        </NextLink>
+        </ParagraphCard>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+        <Button rightIcon={<ChevronRightIcon/>}>
+          My Portfolio
+        </Button>
+          </NextLink>
+        </Box>
+          </Section>
+          <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+        <BioYear>
+          2022 
+        </BioYear>
+        Engineering Prep Fellow at MLH Fellowship
+          </BioSection>
+          <BioSection>
+        <BioYear>
+          2022 to present
+        </BioYear>
+        Frontend Engineer at Acumen Digital
+          </BioSection>
+          </Section>
       </Container>
     )
 }
